57c878ece1696948052c1eb0cc47b7cb4432ad86
Zack Garza <dzackgarza@gmail.com>
2012-05-09 23:31:07 -0700

    More file fixing.

--- src/wavtools/Main.java
+++ src/wavtools/Main.java
@@ -10,7 +10,7 @@ public class Main
    
     public static void main (String[] args)
     {
-    	new SoundProcessor();
+    	s = new SoundProcessor();
         Boolean moretasks = null;
         
         do
